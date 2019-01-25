f9a62d76a0bf48eb125ed24f465e9ad91bf570c8
Faweenagua <46810972+Faweenagua@users.noreply.github.com>
2019-01-25 09:41:05 +0000

    Update Calculator.java

--- JonesFrancisLab2/src/Question1/Calculator.java
+++ JonesFrancisLab2/src/Question1/Calculator.java
@@ -1,4 +1,19 @@
 package Question1;
 
 public class Calculator {
+    double firstNum;
+    double secondNum;
+    double ans;
+    String name;
+
+    public Calculator(){
+
+    }
+
+    public Calculator(String calName){
+        this.name = calName;
+    }
+
+    
+
 }
